1fa7459b58510eba9c4cc0ec0bed60dac8933aff
Aleksej Komnenović <dev@akom.me>
2023-01-10 22:09:55 +0100

    init swift ios app

---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Aleksej Komnenovic on 10.1.23..
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

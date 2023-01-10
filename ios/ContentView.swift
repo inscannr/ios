d9119034206255f697040e39fa4eeb1903ee73ad
Aleksej Komnenović <dev@akom.me>
2023-01-10 22:21:38 +0100

    add code for a simple qr code scanning

--- ios/ContentView.swift
+++ ios/ContentView.swift
@@ -1,21 +1,34 @@
-//
-//  ContentView.swift
-//  ios
-//
-//  Created by Aleksej Komnenovic on 10.1.23..
-//
-
 import SwiftUI
+import CodeScanner
 
 struct ContentView: View {
+    @State var isPresentingScanner = false
+    @State var scannedCode: String = "Scan a QR code:"
+    
+    var scannerSheet : some View {
+        CodeScannerView(
+            codeTypes: [.qr],
+            completion: { result in
+                if case let .success(code) = result {
+                    self.scannedCode = code.string
+                    self.isPresentingScanner = false
+                }
+            }
+        )
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text(scannedCode)
+            
+            Button("Scan a QR code") {
+                self.isPresentingScanner = true
+            }
+            
+            .sheet(isPresented: $isPresentingScanner) {
+                self.scannerSheet
+            }
         }
-        .padding()
     }
 }
 
